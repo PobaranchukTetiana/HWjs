35e882fa6dc320b8024a08250164b008887e05ba
TetianaPobaranchuk <pobaranchuk@gmail.com>
2023-05-07 22:59:39 +0200

    HW

---
+++ 1-variables.js
@@ -0,0 +1,92 @@
+//let number = 5;
+const userScore = 1000;
+
+let number1;
+
+
+// camelCase
+// snake_case 
+// UPPER_SNAKE_CASE
+// kebab-case 
+// PascalCase 
+
+let nUmber = 123;
+
+
+let number = 5;
+console.log(number)
+
+number = 12;
+console.log(number)
+
+
+const userScore1 = 1000;
+console.log(userScore1)
+
+userScore1 = 12;
+console.log(userScore1)
+
+
+
+const obj = {
+    age: 20
+}
+console.log(obj)
+
+// obj = 40;
+// console.log(obj)
+
+obj.age = 30
+console.log(obj)
+
+var firstName = 'Dima'
+
+console.log(firstName)
+
+{
+    let result = 50;
+}
+console.log(result);
+
+{
+    var result1 = 50;
+}
+console.log(result1);
+
+a = 10
+console.log(a)
+
+alert();
+[].push('a')
+
+
+let a = 3000;
+let b = 2000;
+
+/// 100 lines of code
+
+console.log("Ширина авто: " + a + ", довжина: " + b)
+
+let width = 3000;
+let length = 2000
+
+/// 100 lines of code
+
+console.log("Ширина авто: " + width + ", довжина: " + length)
+
+
+let vehicleBodyWidth = 3000;
+let vehicleBodyLength = 2000;
+
+/// 100 lines of code
+
+console.log("Ширина авто: " + vehicleBodyWidth + ", довжина: " + vehicleBodyLength);
+
+
+let carBodyWidth = 3000;
+let machineBodyLength = 2000;
+
+/// 100 lines of code
+
+console.log("Ширина авто: " + carBodyWidth + ", довжина: " + machineBodyLength);
+
